9c14d343fd5fd24a66f81b2d4b1817c4321c53e0
Mike Smullin <mike@smullindesign.com>
2014-12-17 20:54:09 -0700

    messing with async await and Task

--- test.cs
+++ test.cs
@@ -1,22 +1,40 @@
 using System.IO;
 using System;
 using System.Timers;
+using System.Threading.Tasks;
 
 class Program
 {
     static void Main()
     {
-        delay(1000, () => Console.WriteLine("hi"));
-        interval(1300, () => Console.WriteLine("bye"));
-        Console.WriteLine("Hello, World!");
-
-        Console.ReadLine(); // wait for enter
+      notMain();
+      Console.ReadLine(); // wait for enter
     }
 
-    static void delay(int ms, Action fn) {
+    static async void notMain() {
+        Task a = delay(1000, () => Console.WriteLine("a"));
+        //interval(1300, () => Console.WriteLine("bye"));
+        //Task b = delay(2000, () => Console.WriteLine("b"));
+
+        await a;
+
+        Console.WriteLine("Hello, World!");
+
+        //await b;
+
+        Console.WriteLine("Done.");
+
+    }
+
+    static Task delay(int ms, Action fn) {
         Timer tmr = new Timer(ms);
-        tmr.Elapsed += (sender, args) => { fn(); tmr.Stop(); };
+        Task t = new Task(() => {
+          fn();
+          tmr.Stop();
+        });
+        tmr.Elapsed += (sender, args) => { t.Start(); };
         tmr.Start();
+        return t;
     }
 
     static void interval(int ms, Action fn) {
